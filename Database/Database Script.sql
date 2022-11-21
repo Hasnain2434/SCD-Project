1aa5343836aab988cb55a508f3743986f8987863
Hasnain Riaz <mr6100103@gmail.com>
2022-11-21 20:39:09 +0500

    Database Script modified

--- Database/Database Script.sql
+++ Database/Database Script.sql
@@ -5,26 +5,25 @@
 /*CREATE TABLE Mafoul
 (
      رقم int PRIMARY KEY AUTO_INCREMENT,
-    مشكول varchar(50),
-    بغیر_اعراب_مشکول varchar(50),
-    صنف varchar(50),
-    أصل varchar(50),
-    بغیر_اعراب_أصل varchar(50),
-    جنس varchar(50),
-    عدد varchar(50)
+    مشكول varchar(50) null,
+    بغیر_اعراب_مشکول varchar(50) null,
+    صنف varchar(50) null,
+    أصل varchar(50) null,
+    بغیر_اعراب_أصل varchar(50) null,
+    جنس varchar(50) null,
+    عدد varchar(50) null
 );*/
 
 /*CREATE TABLE Faeel
 (
      رقم int PRIMARY KEY AUTO_INCREMENT,
-    مشكول varchar(50),
-    بغیر_اعراب_مشکول varchar(50),
-    صنف varchar(50),
-    أصل varchar(50),
-    بغیر_اعراب_أصل varchar(50),
-    جنس varchar(50),
-    عدد varchar(50),
-    شرح varchar(255)
+    مشكول varchar(50) null,
+    بغیر_اعراب_مشکول varchar(50) null,
+    صنف varchar(50) null,
+    أصل varchar(50) null,
+    بغیر_اعراب_أصل varchar(50) null,
+    جنس varchar(50) null,
+    عدد varchar(50) null
 );*/
 
 /*CREATE TABLE Masdar
